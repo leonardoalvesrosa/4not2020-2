a6b33b33ecbbdd57d02d68c32dc24edc64cfb3a3
leonardoalvesrosa <alvesrosaleonardo14@gmail.com>
2020-10-16 01:19:46 +0000

    (15/10) BowserAnimationsModule

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './ui/principal/principal.component';
